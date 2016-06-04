88ee9b7e7640409b262c75515d0d202f8e3d541a
zncanke <ckzheng0320@outlook.com>
2016-06-04 16:30:37 +0800

    update every 5s finished

--- search/views.py
+++ search/views.py
@@ -1,12 +1,25 @@
 #coding:utf8
+import threading
+from time import sleep
+
 from django.shortcuts import render
 from database.models import StockInfo
 from .models import StockHistoryInfo
 from django.db.models import Q
+from datetime import datetime
 
 # Create your views here.
 
+HasOpened = False
+
 def main(req):
+	global  HasOpened
+	if  not HasOpened:
+		updateDbRegular = UpdateDbRegular()
+		updateDbRegular.setDaemon(True)
+		updateDbRegular.start()
+		HasOpened = True
+
 	return render(req,'stock.html')
 
 def refresh_5s(req):
@@ -39,10 +52,28 @@ def update_realtime(stockcurrentprices):
 		pass
 
 def insert_history(stockhistoryinfo):
-	for stockid in stockhistoryinfo:
-		historytime = stockhistoryinfo[stockid][0]
-		highestvalue = stockhistoryinfo[stockid][1]
-		lowestvalue = stockhistoryinfo[stockid][2]
-		x = StockHistoryInfo(StockID=stockid,HistoryTime=historytime,Highest_value=highestvalue,Lowest_value=lowestvalue)
-		x.save()
-	
+	try:
+		for stockid in stockhistoryinfo:
+			historytime = stockhistoryinfo[stockid][0]
+			highestvalue = stockhistoryinfo[stockid][1]
+			lowestvalue = stockhistoryinfo[stockid][2]
+			x = StockHistoryInfo(StockID=StockInfo.objects.get(StockID="111111"),HistoryTime=historytime,Highest_value=highestvalue,Lowest_value=lowestvalue)
+			x.save()
+	except StockInfo.DoesNotExist:
+		pass
+
+class UpdateDbRegular(threading.Thread):
+
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self.a = 0
+
+	def run(self):
+		while (True):
+			self.a = 111111
+			dt = datetime.now()
+			data = {str(self.a): 12.23}
+			# print(data)
+			update_realtime(data)
+			sleep(5)
+
