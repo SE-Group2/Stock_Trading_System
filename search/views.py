ecce2ff43875f21613d4882cc536ce7110f52451
JeremyJiWZ <mr.ji@icloud.com>
2016-06-28 21:43:36 +0800

    增加了接口设计

--- search/views.py
+++ search/views.py
@@ -8,6 +8,7 @@ from .models import StockHistoryInfo
 from django.db.models import Q
 from datetime import datetime
 from django.http import HttpResponse,Http404
+import json
 
 # Create your views here.
 
@@ -21,8 +22,43 @@ def main(req):
 		updateDbRegular.setDaemon(True)
 		updateDbRegular.start()
 		HasOpened = True
-
-	return render(req,'stock.html')
+	list = [
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:00:00 +0800 2015"},
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:01:00 +0800 2015"},
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:02:00 +0800 2015"},
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:03:00 +0800 2015"},
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:04:00 +0800 2015"},
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:05:00 +0800 2015"},
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:06:00 +0800 2015"},
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:07:00 +0800 2015"},
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:08:00 +0800 2015"},
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:09:00 +0800 2015"},
+		{"volume": 68246139100, "open": 4660.075, "high": 4814.672, "close": 4813.798, "low": 4656.825, "chg": 156.202,
+		 "percent": 3.35, "turnrate": 0.0, "ma5": 4572.931, "ma10": 4461.213, "ma20": 4409.708, "ma30": 4357.837,
+		 "dif": 135.68, "dea": 113.37, "macd": 44.62, "time": "Mon May 25 9:10:00 +0800 2015"}
+	]
+	data = {"symbol": "SHxxxxx", "name": "aaaa", "list": list},
+	return render(req, 'stock.html', {"data": json.dumps(data)})
 
 
 def refresh_5s(req):
