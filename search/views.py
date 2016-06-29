e3ff82d8b75d59b69320f311956670afa94c49f3
JauneWhale <842752622@qq.com>
2016-06-29 20:33:49 +0800

    final v1

--- search/views.py
+++ search/views.py
@@ -20,6 +20,16 @@ import random
 
 HasOpened = False
 
+def JsonWrap(historyinfo):
+	list = []
+	for i in historyinfo:
+		s = i.HistoryTime.strftime("%a %b %d %H:%M:%S +0800 %Y")
+		x = {"volume":i.Volume_value,"open":i.Open_value,"high":i.Highest_value,"close":i.Close_value,"low":i.Lowest_value,"time":s}
+		list.append(x)
+	list.reverse()
+	data = {"symbol":"SHxxxxx","name":"aaaa","list":list}
+	return data
+
 @csrf_exempt
 def main(req):
 	global  HasOpened
@@ -27,39 +37,26 @@ def main(req):
 		updateDbRegular = UpdateDbRegular()
 		updateDbRegular.setDaemon(True)
 		updateDbRegular.start()
-		# initStock()
-		# zao_jia()
 		HasOpened = True
 	if req.method == 'POST':
 		stockinfo = req.POST.get('value')
 		try:
 			x = StockInfo.objects.get(Q(StockID=stockinfo)|Q(StockName=stockinfo))
+			historyinfo = StockHistoryInfo.objects.filter(StockID=x).order_by('-HistoryTime')[:100]
 		except StockInfo.DoesNotExist:
+			pass
+		except StockHistoryInfo.DoesNotExist:
 			pass
 	else:
 		pk = random.randint(1,StockInfo.objects.count())
 		try:
-			x = StockInfo.objects.get(pk=pk)
+			x = StockInfo.objects.all()[pk-1]
+			historyinfo = StockHistoryInfo.objects.filter(StockID=x).order_by('-HistoryTime')[:100]
 		except StockInfo.DoesNotExist:
 			pass
-	res  = json.dumps(JsonResp(x))
-	return render(req,'stock.html',{"stockid":x.StockID,"stockname":x.StockName,"data":res})
-
-
-def JsonResp(x):
-	list = []
-	try:
-		historyinfo = StockHistoryInfo.objects.filter(StockID=x).order_by('-HistoryTime')[:50]
-	except StockHistoryInfo.DoesNotExist:
-		pass
-	else:
-		for i in historyinfo:
-			s = i.HistoryTime.strftime("%a %b %d %H:%M:%S +0800 %Y")
-			x = {"volume":i.Volume_value,"open":i.Open_value,"high":i.Highest_value,"close":i.Close_value,"low":i.Lowest_value,"time":s}
-			list.append(x)
-		list.reverse()
-		data = {"symbol":"SHxxxxx","name":"aaaa","list":list}
-		return data
+		except StockHistoryInfo.DoesNotExist:
+			pass
+	return render(req,'stock.html',{"stockid":x.StockID,"stockname":x.StockName,"data":json.dumps(JsonWrap(historyinfo))})
 
 @csrf_exempt
 def refresh_5s(req):
@@ -70,7 +67,7 @@ def refresh_5s(req):
 		except StockInfo.DoesNotExist:
 			pass
 		else:
-			return JsonResponse({'result':1,'StockID':x.StockID,'CurrentPrice':x.CurrentPrice})
+			return JsonResponse({'result':1,'CurrentPrice':x.CurrentPrice,'A_D':x.A_D,'State':"Normal"})
 	res = {}
 	res["result"] = 0
 	return JsonResponse(res)
@@ -81,16 +78,17 @@ def refresh_1min(req):
 		stockinfo = req.POST.get('value')
 		try:
 			x = StockInfo.objects.get(Q(StockID=stockinfo)|Q(StockName=stockinfo))
+			historyinfo = StockHistoryInfo.objects.filter(StockID=x).order_by('-HistoryTime')[:100]
 		except StockInfo.DoesNotExist:
 			pass
 		except StockHistoryInfo.DoesNotExist:
 			pass
 		else:
-			res = JsonResp(x)
-			return JsonResponse({"result":1,"data":res})
-	res = {}
-	res["result"] = 0
-	return JsonResponse(res)
+			return JsonResponse({"result":1,"data":JsonWrap(historyinfo)})
+	else:
+		res = {}
+		res["result"] = 0
+		return JsonResponse(res)
 
 
 def search_history(req):
@@ -99,123 +97,49 @@ def search_history(req):
 		starttime = req.POST.get('starttime')
 		endtime = req.POST.get('endtime')
 		try:
-			x = StockHistoryInfo.objects.filter(StockID=stockid,HistoryTime__gte=starttime,HistoryTime__lte=endtime)
+			historyinfo = StockHistoryInfo.objects.filter(StockID=stockid,HistoryTime__gte=starttime,HistoryTime__lte=endtime)
 		except StockInfo.DoesNotExist:
 			pass
 		else:
-			return HttpResponse({'history_info':x})
+			return JsonResponse({"result":1,'history_info':JsonWrap(historyinfo)})
 	else:
-		raise Http404
+		res = {}
+		res["result"] = 0
+		return JsonResponse(res)
 
-
-def update_realtime(stockcurrentprices):
+def update_realtime(stockcurrentdata):
 	try:
-		for stockid in stockcurrentprices:
+		for stockid in stockcurrentdata:
 			x = StockInfo.objects.get(StockID=stockid)
-			x.CurrentPrice = stockcurrentprices[stockid]
+			x.CurrentPrice = stockcurrentdata[stockid][0]
+			x.A_D = stockcurrentdata[stockid][1]
 			x.save()
 	except StockInfo.DoesNotExist:
 		pass
 
 
 def insert_history(stockhistoryinfo):
+	print stockhistoryinfo
 	try:
 		for stockid in stockhistoryinfo:
+			t = StockInfo.objects.get(StockID=stockid)
+			last_record = StockHistoryInfo.objects.filter(StockID=t).order_by('-HistoryTime')[0]
 			historytime = stockhistoryinfo[stockid][0]
-			highestvalue = stockhistoryinfo[stockid][1]
-			lowestvalue = stockhistoryinfo[stockid][2]
-
-			x = StockHistoryInfo(StockID=StockInfo.objects.get(StockID="111111"),HistoryTime=historytime,Highest_value=highestvalue,Lowest_value=lowestvalue)
+			openvalue = stockhistoryinfo[stockid][1]
+			closevalue = stockhistoryinfo[stockid][2]
+			highestvalue = stockhistoryinfo[stockid][3]
+			lowestvalue = stockhistoryinfo[stockid][4]
+			volume = stockhistoryinfo[stockid][5]
+			EMA12 = last_record.EMA12*11/13 + closevalue*2/13
+			EMA26 = last_record.EMA12*25/27 + closevalue*2/27
+			DIF = EMA12 - EMA26
+			MACD = last_record.MACD*8/10 + DIF*2/10
+			x = StockHistoryInfo(StockID=t,HistoryTime=historytime,Open_value=openvalue,Close_value=closevalue,Highest_value=highestvalue,Lowest_value=lowestvalue,Volume_value=volume,EMA12=EMA12,EMA26=EMA26,DIF=DIF,MACD=MACD)
 			x.save()
 	except StockInfo.DoesNotExist:
 		pass
-
-
-def zao_jia():
-	dt = datetime.now()
-	a = random.random() * 10 + 5
-	b = random.random() * 10 + 5
-	c = random.random() * 10 + 5
-	d = random.random() * 10 + 5
-	a = b
-	b = random.random() * 10 + 5
-	c = random.random() * 10 + 5
-	d = random.random() * 10 + 5
-	dt = dt.replace(hour=int(dt.hour) / 60, minute=int(dt.minute) % 60)
-	x = StockHistoryInfo(StockID=StockInfo.objects.get(StockID="111111"), HistoryTime=dt, Open_value=a,
-							 Close_value=b, Highest_value=max(a, b, c, d), Lowest_value=min(a, b, c, d),
-							 Volume_value=1000, EMA12=0, EMA26=0, DIF=0, MACD=0)
-	print x.HistoryTime
-	x.save()
-
-	a = b
-	b = random.random() * 10 + 5
-	c = random.random() * 10 + 5
-	d = random.random() * 10 + 5
-	dt = dt.replace(hour=int(dt.hour) / 60, minute=int(dt.minute) % 60)
-	x = StockHistoryInfo(StockID=StockInfo.objects.get(StockID="222222"), HistoryTime=dt, Open_value=a,
-							 Close_value=b, Highest_value=max(a, b, c, d), Lowest_value=min(a, b, c, d),
-							 Volume_value=1000, EMA12=0, EMA26=0, DIF=0, MACD=0)
-	# x.save()
-
-	a = b
-	b = random.random() * 10 + 5
-	c = random.random() * 10 + 5
-	d = random.random() * 10 + 5
-	dt = dt.replace(hour=int(dt.hour) / 60, minute=int(dt.minute) % 60)
-	x = StockHistoryInfo(StockID=StockInfo.objects.get(StockID="333333"), HistoryTime=dt, Open_value=a,
-							 Close_value=b, Highest_value=max(a, b, c, d), Lowest_value=min(a, b, c, d),
-							 Volume_value=1000, EMA12=0, EMA26=0, DIF=0, MACD=0)
-	# x.save()
-
-
-def initStock():
-
-	x = StockInfo(StockID="111111", StockName="baidu", CurrentPrice=10.3, A_D=0.2, UpLimit=100, DownLimit=1)
-	x.save()
-	x = StockInfo(StockID="222222", StockName="alipp", CurrentPrice=11.3, A_D=0.3, UpLimit=100, DownLimit=1)
-	x.save()
-	x = StockInfo(StockID="333333", StockName="qq", CurrentPrice=12.3, A_D=0.1, UpLimit=100, DownLimit=1)
-	x.save()
-
-	dt = datetime.now()
-
-	a = random.random() * 10 + 5
-	b = random.random() * 10 + 5
-	c = random.random() * 10 + 5
-	d = random.random() * 10 + 5
-	for i in xrange(1, 20):
-		a = b
-		b = random.random() * 10 + 5
-		c = random.random() * 10 + 5
-		d = random.random() * 10 + 5
-		dt = dt.replace(hour=i / 60, minute=i % 60)
-		x = StockHistoryInfo(StockID=StockInfo.objects.get(StockID="111111"), HistoryTime=dt, Open_value=a,
-							 Close_value=b, Highest_value=max(a, b, c, d), Lowest_value=min(a, b, c, d),
-							 Volume_value=1000, EMA12=0, EMA26=0, DIF=0, MACD=0)
-		x.save()
-
-	for i in xrange(1, 20):
-		a = b
-		b = random.random() * 10 + 5
-		c = random.random() * 10 + 5
-		d = random.random() * 10 + 5
-		dt = dt.replace(hour=i / 60, minute=i % 60)
-		x = StockHistoryInfo(StockID=StockInfo.objects.get(StockID="222222"), HistoryTime=dt, Open_value=a,
-							 Close_value=b, Highest_value=max(a, b, c, d), Lowest_value=min(a, b, c, d),
-							 Volume_value=1000, EMA12=0, EMA26=0, DIF=0, MACD=0)
-		x.save()
-
-	for i in xrange(1, 20):
-		a = b
-		b = random.random() * 10 + 5
-		c = random.random() * 10 + 5
-		d = random.random() * 10 + 5
-		dt = dt.replace(hour=i / 60, minute=i % 60)
-		x = StockHistoryInfo(StockID=StockInfo.objects.get(StockID="333333"), HistoryTime=dt, Open_value=a,
-							 Close_value=b, Highest_value=max(a, b, c, d), Lowest_value=min(a, b, c, d),
-							 Volume_value=1000, EMA12=0, EMA26=0, DIF=0, MACD=0)
-		x.save()
+	except StockHistoryInfo.DoesNotExist:
+		pass
 
 class UpdateDbRegular(threading.Thread):
 
@@ -224,10 +148,47 @@ class UpdateDbRegular(threading.Thread):
 		self.a = 0
 
 	def run(self):
+		count = 0;
+		stockid = ["111111","222222","333333"]
+		openvalue = []
+		closevalue = []
+		maxvalue = []
+		minvalue = []
+		volume = []
+		data = {}
+		currentprice = []
+		print "Thread start"
+		for i in xrange(3):
+			openvalue.append(10)
+			closevalue.append(10)
+			maxvalue.append(0)
+			minvalue.append(100)
+			volume.append(0)
+			currentprice.append(10)
+
 		while (True):
-			self.a = 111111
-			dt = datetime.now()
-			data = {str(self.a): 12.23}
-			zao_jia()
-			# update_realtime(data)
-			sleep(5)
+			for i in xrange(3):
+				A_D = random.random()*0.2-0.1
+				if currentprice[i] <= 5 and A_D <0:
+					A_D = -A_D;
+				rd = A_D*currentprice[i]
+				currentprice[i] = currentprice[i] + rd
+				data[stockid[i]] = [currentprice[i],A_D]
+				if currentprice[i] > maxvalue[i]:
+					maxvalue[i] = currentprice[i]
+				if currentprice[i] < minvalue[i]:
+					minvalue[i] = currentprice[i]
+			print data
+			update_realtime(data)
+
+			if count%12==11:
+				dt = datetime.now()
+				for i in xrange(3):
+					openvalue[i] = closevalue[i]
+					closevalue[i] = currentprice[i]
+					volume[i] = random.randint(100,1000)
+					insert_history({stockid[i]:[dt,openvalue[i],closevalue[i],maxvalue[i],minvalue[i],volume[i]]})
+					maxvalue[i] = 0
+					minvalue[i] = 100
+			count = count + 1
+			sleep(0.1)
