78ba90110324c99490827ae7bf3e4821ac7b7fe4
JauneWhale <842752622@qq.com>
2016-06-30 14:15:36 +0800

    a

--- search/admin.py
+++ search/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import StockHistoryInfo
+from .models import StockHistoryInfo, StockInfo
 
 # Register your models here.
 admin.site.register(StockHistoryInfo)
+admin.site.register(StockInfo)
